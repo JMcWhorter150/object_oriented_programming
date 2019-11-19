e33e99445d91c791e737acaefb5afd095fadb836
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-11-19 11:14:13 -0500

    Adds toys and toy methods for sloth

--- sloths.py
+++ sloths.py
@@ -17,15 +17,34 @@ class Sloth:
             whom.happiness += self.cuddle_power
         else:
             print(f"Bad touch, bad touch!")
+    
+    def receive_toy(self, toy):
+        self.toy = toy
+
+    def play_with_toy(self):
+        print(f"{self.name} plays with {self.toy.name}")
+        self.happiness += self.toy.quality
+
 
 class SuperSloth(Sloth):
+    def __init__(self, new_name):
+        super().__init__(new_name, 0.9, 50, 5)
+
     def cuddle(self, whom):
+        # as long as whom has a .name and a .happiness, cuddle method can do its work.
         cuddle_chance = randint(0, 10)/10
         if cuddle_chance <= self.friendliness:
             print(f"I cuddle you, {whom.name}")
             whom.happiness *= self.cuddle_power
         else:
             print(f"Bad touch, bad touch!")
+
+class Toy:
+    def __init__(self, name, quality):
+        self.name = name
+        self.quality = quality
+
+    
 
 buster = Sloth("Buster")
 eric = Sloth("Eric")
